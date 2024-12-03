463c86ebaeae9e9c7b8cbe7f8378dca22cec4873
antarikshgosain <antariksh.gosain@gmail.com>
2024-12-03 00:48:24 -0500

    created dockerfile for nodejs-crud using Docker command palette

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js');
+
+//middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+mongoose.connect("mongodb+srv://anta:anta01@node-cluster-1.c9d6e.mongodb.net/nodedb?retryWrites=true&w=majority&appName=node-cluster-1")
+.then(() => {
+    console.log('Connected to Mongo Database');
+    app.listen(3000, () => {
+        console.log('Server is running on port: 3000');
+    });
+})
+.catch(() => {
+    console.log('Connection to Mongo Database Failed');
+});
+
+app.get('/', (req,res) => {
+    res.send('Hello from node.js API server');
+});
+
+//route(s)
+app.use("/api/product", productRoute);
+
